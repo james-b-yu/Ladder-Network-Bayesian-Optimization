93a3f6b1d0a41eafb251d8e2be1a5e49e971e68e
James Yu <james-yu@outlook.com>
2022-09-13 17:06:18 +0100

    add confusion matrix

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from torch.utils.data import Dataset, DataLoader, Subset
 import time
 from model import AutoEncoderFModule, AutoEncoderModel
 
-from ray import tune
-
 
 class AutoEncoderHyperParameters(TypedDict):
     learning_rate: float  # MUST BE A POWER OF 2. eg learning_rate of -2 means 0.01
@@ -34,28 +32,38 @@ class AutoEncoderConfig(TypedDict):
     optim: Any
 
 
-class AutoEncoder():
+class AutoEncoder:
     def init_dataloaders(self):
         batch_size = max(16, int(2 ** self.hp["batch_size"]))
 
-        self.train_dataloader = DataLoader(self.cfg["train_dataset"], batch_size=batch_size, shuffle=self.cfg["shuffle"])
-        self.validation_dataloader = DataLoader(self.cfg["test_dataset"], batch_size=batch_size, shuffle=self.cfg["shuffle"])
+        self.train_dataloader = DataLoader(
+            self.cfg["train_dataset"],
+            batch_size=batch_size,
+            shuffle=self.cfg["shuffle"],
+        )
+        self.validation_dataloader = DataLoader(
+            self.cfg["test_dataset"], batch_size=batch_size, shuffle=self.cfg["shuffle"]
+        )
 
     def init_device(self):
         self.device = t.device("cpu")
 
     def init_model_hps(self, m: nn.Module):
-        if (isinstance(m, AutoEncoderFModule) or isinstance(m, AutoEncoderModel)):
+        if isinstance(m, AutoEncoderFModule) or isinstance(m, AutoEncoderModel):
             m.corruption_std = 10 ** self.hp["ladder_corruption_std"]
         return
 
     def init_model(self):
-        self.model: nn.Module = self.cfg["model"](self.cfg["input_shape"], self.cfg["output_classes"]).to(self.device)
+        self.model: nn.Module = self.cfg["model"](
+            self.cfg["input_shape"], self.cfg["output_classes"]
+        ).to(self.device)
         self.model.apply(self.init_model_hps)
 
     def init_optim(self):
         lr = 10 ** self.hp["learning_rate"]
-        self.optim: t.optim.Optimizer = self.cfg["optim"](self.model.parameters(), lr=lr)
+        self.optim: t.optim.Optimizer = self.cfg["optim"](
+            self.model.parameters(), lr=lr
+        )
 
     def __init__(self, cfg: AutoEncoderConfig, hp: AutoEncoderHyperParameters):
         # init hyperparameters
@@ -71,21 +79,40 @@ class AutoEncoder():
         self.init_dataloaders()
 
     def go(self):
-        totalLoss, totalAccuracy = t.zeros(self.cfg["epochs"], device=self.device), t.zeros(self.cfg["epochs"], device=self.device)
+        losses, accuracies = t.zeros(
+            self.cfg["epochs"], dtype=t.float64, device=self.device
+        ), t.zeros(self.cfg["epochs"], dtype=t.float64, device=self.device)
+
+        num_examples = len(cast(Sized, self.validation_dataloader.dataset))
+        metricss = t.zeros((self.cfg["epochs"], num_examples, 3), dtype=t.float64, device=self.device)
+        confusion_matrices = t.zeros((self.cfg["epochs"], self.cfg["output_classes"], self.cfg["output_classes"]), dtype=t.int64, device=self.device)
+
+        
+
 
         for epoch in range(self.cfg["epochs"]):
             start_time = time.perf_counter()
             self.one_epoch(False, epoch)
-            loss, accuracy = self.one_epoch(True, epoch)
-            totalLoss[epoch] = loss
-            totalAccuracy[epoch] = accuracy
+            loss, accuracy, metrics, confusion_matrix = self.one_epoch(True, epoch)
+            losses[epoch] = loss
+            accuracies[epoch] = accuracy
+            metricss[epoch] = metrics
+            confusion_matrices[epoch] = confusion_matrix
+
             end_time = time.perf_counter()
             print("process took {:.2f} secs to complete".format(end_time - start_time))
 
-        return totalLoss, totalAccuracy
+        return losses, accuracies, metricss, confusion_matrices
 
     def ladder_weight(self, epoch: int):
-        return (10 ** self.hp["ladder_loss_saturation"]) * t.sigmoid(t.tensor([(10 ** self.hp["ladder_loss_acceleration"]) * (epoch - self.hp["ladder_loss_delay"])])).to(self.device)
+        return (10 ** self.hp["ladder_loss_saturation"]) * t.sigmoid(
+            t.tensor(
+                [
+                    (10 ** self.hp["ladder_loss_acceleration"])
+                    * (epoch - self.hp["ladder_loss_delay"])
+                ]
+            )
+        ).to(self.device)
 
     def one_epoch(self, val: bool, epoch):
         if val:
@@ -102,6 +129,7 @@ class AutoEncoder():
         totalAccuracy = t.tensor([0.0], device=self.device)
 
         metrics = t.zeros((num_examples, 3))
+        confusion_matrix = t.zeros((self.cfg["output_classes"], self.cfg["output_classes"]), dtype=t.int64)
 
         for index, batch in enumerate(dl):
             if not val:
@@ -139,39 +167,21 @@ class AutoEncoder():
                 metrics[batch_start:batch_end, 2] = logit_loss
 
                 accuracy = (y == predictions).sum() / y.size(0)
+                confusion_matrix[y, predictions] += 1
 
                 totalAccuracy += (y == predictions).sum() / num_examples
 
                 if index % 100 == 0:
-                    print("epoch {}/{}:\t{}\tbatch: {}/{}\tloss: {:.2f}\taccuracy: {:.2f}".format(epoch, self.cfg["epochs"], "val" if val else "train", index, num_batches, mean_logit_loss, accuracy))
+                    print(
+                        "epoch {}/{}:\t{}\tbatch: {}/{}\tloss: {:.2f}\taccuracy: {:.2f}".format(
+                            epoch,
+                            self.cfg["epochs"],
+                            "val" if val else "train",
+                            index,
+                            num_batches,
+                            mean_logit_loss,
+                            accuracy,
+                        )
+                    )
 
-        return totalLoss, totalAccuracy
-
-
-# cfg: AutoEncoderConfig = {
-#     "input_shape": (28, 28),
-#     "output_classes": 47,
-#     "model": AutoEncoderModel,
-#     "train_dataset": Subset(datasets.EMNIST("data/", train=True, split="balanced", download=True, transform=transforms.Compose([
-#         transforms.ToTensor(),
-#         transforms.Normalize((0.1307), (0.3081))
-#     ])), range(6400 * 2)),
-#     "test_dataset": Subset(datasets.EMNIST("data/", train=False, split="balanced", download=True, transform=transforms.Compose([
-#         transforms.ToTensor(),
-#         transforms.Normalize((0.1307), (0.3081))
-#     ])), range(6400 * 2)),
-#     "epochs": 100,
-#     "optim": t.optim.Adam,
-#     "shuffle": True
-# }
-
-# hp: AutoEncoderHyperParameters = {
-#     "batch_size": 256,
-#     "learning_rate": 1e-2,
-#     "ladder_loss_acceleration": 1e-1,
-#     "ladder_loss_saturation": 1e-1,
-#     "ladder_corruption_std": 1e-1
-# }
-
-# a = AutoEncoder(cfg, hp)
-# a.go()
+        return totalLoss, totalAccuracy, metrics, confusion_matrix
